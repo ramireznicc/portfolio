8680774eb95f726090ad67f7fdfdf2e2e1c85252
Nico Ramirez <ramireznicc.23@gmail.com>
2023-07-11 21:38:33 +0200

    [feature] implement resume section

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,7 +1,14 @@
+import "./Resume.css";
+
 const Resume = () => {
   return (
-    <div>
-      <h1>Resume</h1>
+    <div className="container">
+      <iframe
+        title="resume"
+        src="https://drive.google.com/file/d/113-jqlah5MXG6BeaTpf0JJ0p6wfcx0SR/preview"
+        frameborder="0"
+        className="resume"
+      />
     </div>
   );
 };
